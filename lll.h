de4424c8f658ebeb42186e445a41df740c284374
Steven Ngo <stevenngo36@yahoo.com>
2018-04-18 15:17:21 -0700

    Update lll.h

--- lll.h
+++ lll.h
@@ -12,5 +12,12 @@ class lll(){
     ~lll();
     
     int insert(int to_Add);
-    int delete(int to_Remove);
+    int remove(int to_Remove);
+    void display();
+  
+  private:
+    node * head;
+    int insert(node*&head, int to_Add);
+    int remove(node*&head, int to_Remove);
+    void display(node * head);
 };
